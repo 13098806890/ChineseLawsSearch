949b6523964302425fc2f1a5d4409c5979333fdb
Xie, Dongze <doxie@strategy.com>
2026-04-29 14:39:58 +0800

    Initial Commit

---
+++ ChineseLawsSearchTests/ChineseLawsSearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChineseLawsSearchTests.swift
+//  ChineseLawsSearchTests
+//
+//  Created by Xie, Dongze on 2026/4/29.
+//
+
+import Testing
+@testable import ChineseLawsSearch
+
+struct ChineseLawsSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
